b0696dcf60c9cc2c48fd51dc584296bfa18f7184
Samantha Mashaal <Smash@Samanthas-MacBook-Pro-2.local>
2016-06-06 16:46:56 +0200

    Day 6 morning JS-Koans exercises

--- topics/about_functions_and_closure.js
+++ topics/about_functions_and_closure.js
@@ -7,14 +7,14 @@ test("defining functions directly", function() {
         result = "b";
     };
     changeResult();
-    equal(__, result, 'what is the value of result?');
+    equal("b", result, 'what is the value of result?');
 });
 
 test("assigning functions to variables", function() {
     var triple = function(input) {
         return input * 3;
     };
-    equal(__, triple(4), 'what is triple 4?');
+    equal(12, triple(4), 'what is triple 4?');
 });
 
 test("self invoking functions", function() {
@@ -23,7 +23,7 @@ test("self invoking functions", function() {
     // self invoking functions are used to provide scoping and to alias variables
     (function(pv) {
         var secretValue = "password";
-        equal(__, pv, 'what is the value of pv?');
+        equal("password", pv, 'what is the value of pv?');
         equal("__", typeof(secretValue), "is secretValue available in this context?");
         equal("__", typeof(publicValue), "is publicValue available in this context?");
     })(publicValue);
